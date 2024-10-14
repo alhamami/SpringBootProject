2409060816645264c010e6d4e1fe3adfba3dc137
alhamami <89762635+alhamami@users.noreply.github.com>
2024-10-14 08:23:40 +0300

    Create Student.java

---
+++ src/main/java/com/jalal/SpringProject/model/Student.java
@@ -0,0 +1,40 @@
+package com.example.springProject.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
+
+@Component
+@Scope("prototype")
+public class Student{
+
+  private int rollNo;
+  private String name;
+  private int marks;
+
+  public int getRollNo() {
+    return rollNo;
+  }
+
+  public void setRollNo(int rollNo) {
+    this.rollNo = rollNo;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getMarks() {
+    return marks;
+  }
+
+  public void setMarks(int marks) {
+    this.marks = marks;
+  }
+  
+
+  
+}
